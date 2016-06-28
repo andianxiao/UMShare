82b76e9a7459e9cde13d26d0044a2095969bb043
安潇 <andianxiao@126.com>
2016-06-28 11:15:10 +0800

    初始化UMShareTest项目

---
+++ UMShareTest/UMShareTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UMShareTest
+//
+//  Created by 安潇 on 16/6/28.
+//  Copyright © 2016年 安潇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
